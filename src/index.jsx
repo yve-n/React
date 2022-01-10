c0dfe5600d3ed202295952d4057f3c5ef6d66dfd
yve-n <nguetcheyvecarelle@gmail.com>
2022-01-10 13:57:06 +0100

    initial test create component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React , {Component} from 'react';
 import ReactDom from 'react-dom';
 
-function Person(){
-    let name = 'yves';
-    return <h2>{name}</h2>
-}
+// function Person(){
+//     let name = 'yves';
+//     return (
+//     <>
+//         <h1>je suis </h1>
+//         <h2>{name}</h2>
+//     </>
+//     )
+// }
 
-console.log(Person());
+// class Person extends Component{
+//     name = 'noam';
+//     render(){
+//         return(
+//             <>
+//             <h2>{this.name}</h2>
+
+//             </>
+//         )
+//     }
+// }
+
 ReactDom.render(<Person />, document.getElementById('root'));
