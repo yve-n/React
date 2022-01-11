3c4acdda875cc10791579c0fa7dd3758a7174074
yve-n <nguetcheyvecarelle@gmail.com>
2022-01-11 12:02:13 +0100

    useEffects et data.js

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,26 @@
 import { useEffect, useState } from "react";
+import {persons} from '../data';
 const Home = () => {
     const [name, setName] = useState('yve');
     const [isYve, setIsYve]= useState(false);
+    const [isAmel, setAmel] = useState(true);
+
+    const [ps, setPersons] = useState([]);
 
     useEffect(()=>{
         setName(name === 'Yve' ? 'Amel' : 'Yve');
     },[isYve]);
+    
+    useEffect(()=>{
+        setPersons(persons);
+    },[]);
 
-    return <h3 onClick={()=>{setIsYve(!isYve)}}>{name}</h3>
+    return (
+        <>
+            {ps.map(person=> <p key={person.age}>{person.name}</p>)}
+            <h3 onClick={()=>{setIsYve(!isYve)}}>{name}</h3>
+
+        </>
+    )
 }
 export default Home;
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const persons = [
+    { "name": "Yve", "age": 25 },
+    { "name": "Amel", "age": 4 },
+    { "name": "Noam", "age": 1 }
+  ];
