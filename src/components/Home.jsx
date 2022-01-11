f0cc8e3711895d61ac4a2047187e96a0f2b428c8
yve-n <nguetcheyvecarelle@gmail.com>
2022-01-11 12:32:57 +0100

    requete ajax

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from "react";
 import {persons} from '../data';
 const Home = () => {
-    const [name, setName] = useState('yve');
-    const [isYve, setIsYve]= useState(false);
-    const [isAmel, setAmel] = useState(true);
 
-    const [ps, setPersons] = useState([]);
-
+    const [name, setName] = useState('');
     useEffect(()=>{
-        setName(name === 'Yve' ? 'Amel' : 'Yve');
-    },[isYve]);
-    
-    useEffect(()=>{
-        setPersons(persons);
+        fetch('http://localhost:5000/profile')
+    .then(response => response.json())
+    .then(profile => setName(profile.name))
+    .catch(error =>console.error(error));
     },[]);
+    
+    // const [isYve, setIsYve]= useState(false);
+    // const [isAmel, setAmel] = useState(true);
+
+    // const [ps, setPersons] = useState([]);
+
+    // useEffect(()=>{
+    //     setName(name === 'Yve' ? 'Amel' : 'Yve');
+    // },[isYve]);
+
+    // useEffect(()=>{
+    //     setPersons(persons);
+    // },[]);
 
     return (
         <>
-            {ps.map(person=> <p key={person.age}>{person.name}</p>)}
-            <h3 onClick={()=>{setIsYve(!isYve)}}>{name}</h3>
-
+            {/* {ps.map(person=> <p key={person.age}>{person.name}</p>)}
+            <h3 onClick={()=>{setIsYve(!isYve)}}>{name}</h3> */}
+            { <h2>Hello {name}</h2>}
         </>
     )
 }
