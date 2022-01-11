2c60a8147d29cb4cc5b1039a74ce7783c98445ff
yve-n <nguetcheyvecarelle@gmail.com>
2022-01-11 10:20:18 +0100

    tp color

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,30 @@ import { useState } from "react";
 
 const App = () => {  
     const names =['noam', 'amel','yve'];
+    
+
+    let [currentActive, setCurrentActive] = useState(0);
+    const ChangeColor = () =>{
+        if(currentActive === names.length - 1){
+            setCurrentActive(0);
+            return;
+        }
+        setCurrentActive(currentActive + 1);
+
+    }
+   
+    
 
 return (
     <>
-        <button>select next name</button>
-       <ul style={{backgroundColor: 'aqua'}}>
-           {names.map((name,index) =><li key={index}>{name}</li>)}
+        <button onClick={ChangeColor}>select next name</button>
+       <ul style={{backgroundColor:'aqua'}}>
+           {names.map((name,index) =><li 
+           style ={{backgroundColor :currentActive ===index ?'blue' : "transparent"}}
+           key={index} >{name}</li>)}
        </ul>
+       
+        
     </>
 )
 
