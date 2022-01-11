cfd2a930e9f811f8c420220d631f993e36ec515a
yve-n <nguetcheyvecarelle@gmail.com>
2022-01-11 11:49:06 +0100

    useEffects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,2 +1,12 @@
-const Home = () => <h1>Hello Home page</h1>
+import { useEffect, useState } from "react";
+const Home = () => {
+    const [name, setName] = useState('yve');
+    const [isYve, setIsYve]= useState(false);
+
+    useEffect(()=>{
+        setName(name === 'Yve' ? 'Amel' : 'Yve');
+    },[isYve]);
+
+    return <h3 onClick={()=>{setIsYve(!isYve)}}>{name}</h3>
+}
 export default Home;
